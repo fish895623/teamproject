2c8274e53a8e7284cd834b3cf849d21404130b38
fish895623 <dan990429@gmail.com>
2025-01-03 17:44:36 +0900

    feat: Add Eclipse project configuration files

---
+++ src/teamproject2/App.java
@@ -0,0 +1,8 @@
+package teamproject2;
+
+public class App {
+	/** @apiNote Entry point */
+	public static void main(String[] args) {
+		System.out.println("asdf");
+	}
+}
---
+++ src/teamproject2/Device.java
@@ -0,0 +1,7 @@
+package teamproject2;
+
+public class Device {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
