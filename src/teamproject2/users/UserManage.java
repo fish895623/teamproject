bb5baa1e1b5dfdec8d3b1aa3cceb68781c2fcf69
KatherineJH <KatherineJH@users.noreply.github.com>
2025-01-06 12:42:26 +0000

    FORMAT!!!

--- src/teamproject2/users/UserManage.java
+++ src/teamproject2/users/UserManage.java
@@ -6,78 +6,78 @@ import java.util.List;
 import java.util.Map;
 
 public class UserManage {
-    // data members
-    private List<Map<String, User>> userList;
+	// data members
+	private List<Map<String, User>> userList;
 
-    // constructor
-    public UserManage() {
-        this.userList = new ArrayList<>(); // initiate
-    }
+	// constructor
+	public UserManage() {
+		this.userList = new ArrayList<>(); // initiate
+	}
 
-    // methods
-    // add user
-    public void addUser(String userId, String userPassword, String userName, String email) {
-        User newUser = new User(userId, userPassword, userName, email);
-        if (userList.isEmpty()) {
-            Map<String, User> userGroup = new HashMap<>();
-            userGroup.put(userId, newUser);
-            userList.add(userGroup);
-        } else {
-            Map<String, User> userGroup = userList.get(userList.size() - 1);
-            userGroup.put(userId, newUser);
-        }
+	// methods
+	// add user
+	public void addUser(String userId, String userPassword, String userName, String email) {
+		User newUser = new User(userId, userPassword, userName, email);
+		if (userList.isEmpty()) {
+			Map<String, User> userGroup = new HashMap<>();
+			userGroup.put(userId, newUser);
+			userList.add(userGroup);
+		} else {
+			Map<String, User> userGroup = userList.get(userList.size() - 1);
+			userGroup.put(userId, newUser);
+		}
 
-        System.out.println("User " + userName + " added successfully.");
-    }
+		System.out.println("User " + userName + " added successfully.");
+	}
 
-    // delete user
-    public void deleteUser(String userId) {
-        boolean userFound = false;
+	// delete user
+	public void deleteUser(String userId) {
+		boolean userFound = false;
 
-        for (Map<String, User> userGroup : userList) {
-            if (userGroup.containsKey(userId)) {
-                userGroup.remove(userId);
-                userFound = true;
-                System.out.println("User " + userId + " deleted successfully.");
-                return;
-            }
-        }
+		for (Map<String, User> userGroup : userList) {
+			if (userGroup.containsKey(userId)) {
+				userGroup.remove(userId);
+				userFound = true;
+				System.out.println("User " + userId + " deleted successfully.");
+				return;
+			}
+		}
 
-        if (!userFound) {
-            System.out.println("User with ID " + userId + " not found.");
-        }
-    }
+		if (!userFound) {
+			System.out.println("User with ID " + userId + " not found.");
+		}
+	}
 
-    // edit password
-    public void modifyUser(String userId, String userPassword, String newPassword) {
-        boolean userFound = false;
+	// edit password
+	public void modifyUser(String userId, String userPassword, String newPassword) {
+		boolean userFound = false;
 
-        for (Map<String, User> userGroup : userList) {
-            if (userGroup.containsKey(userId)) {
-                User user = userGroup.get(userId);
-                // check current password corresponding
-                if (user.getUserPassword().equals(userPassword)) {
-                    user.setUserPassword(newPassword);
-                    userFound = true;
-                    System.out.println("User " + userId + "'s password updated successfully.");
-                    return;
-                } else {
-                    System.out.println("Incorrect current password for user " + userId + ".");
-                    return;
-                }
-            }
-        }
-        if (!userFound) {
-            System.out.println("User with ID " + userId + " not found.");
-        }
-    }
+		for (Map<String, User> userGroup : userList) {
+			if (userGroup.containsKey(userId)) {
+				User user = userGroup.get(userId);
+				// check current password corresponding
+				if (user.getUserPassword().equals(userPassword)) {
+					user.setUserPassword(newPassword);
+					userFound = true;
+					System.out.println("User " + userId + "'s password updated successfully.");
+					return;
+				} else {
+					System.out.println("Incorrect current password for user " + userId + ".");
+					return;
+				}
+			}
+		}
+		if (!userFound) {
+			System.out.println("User with ID " + userId + " not found.");
+		}
+	}
 
-    // getter and setter
-    public List<Map<String, User>> getUserList() {
-        return userList;
-    }
+	// getter and setter
+	public List<Map<String, User>> getUserList() {
+		return userList;
+	}
 
-    public void setUserList(List<Map<String, User>> userList) {
-        this.userList = userList;
-    }
+	public void setUserList(List<Map<String, User>> userList) {
+		this.userList = userList;
+	}
 }
