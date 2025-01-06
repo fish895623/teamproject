6a1ce555c0a06a04012e413695a56a62b5a66507
KatherineJH <ohhomoya@gmail.com>
2025-01-06 11:01:26 +0900

    admin modified

--- src/teamproject2/users/Admin.java
+++ src/teamproject2/users/Admin.java
@@ -2,7 +2,7 @@ package teamproject2.users;
 
 public class Admin extends User {
 	// field
-	private String role;
+	private int role; // 0.  device manager, 1. user manager
 
 	// constructor
 	public Admin(String userId, String userPassword, String userName, String email) {
@@ -10,20 +10,30 @@ public class Admin extends User {
 		
 	}
 	
-	// method
+	// method	
 	public void manageDevice() {
+		if(role== 0) {
+			System.out.println("Device manager approved.");
+		}else {
+			System.out.println("You can not access to this job.");
+		}
 	}
 
 	public void manageUser() {
+		if(role== 1) {
+			System.out.println("User manager approved.");
+		}else {
+			System.out.println("You can not access to this job.");			
+		}
 	}
 
 
 	// getter and setter
-	public String getRole() {
+	public int getRole() {
 		return role;
 	}
 
-	public void setRole(String role) {
+	public void setRole(int role) {
 		this.role = role;
 	}
 }
