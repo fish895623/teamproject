921f6cb3af39fd9e7b61e1488f6444fa3b57c8d6
KatherineJH <KatherineJH@users.noreply.github.com>
2025-01-06 12:36:43 +0000

    FORMAT!!!

--- src/teamproject2/users/DeviceManage.java
+++ src/teamproject2/users/DeviceManage.java
@@ -9,88 +9,88 @@ import java.util.List;
 import java.util.Map;
 
 public class DeviceManage {
-    // data members
-    private List<Map<Device, Integer>> deviceStock;
+	// data members
+	private List<Map<Device, Integer>> deviceStock;
 
-    // constructor
-    public DeviceManage() {
-        this.deviceStock = new java.util.ArrayList<>(); // initiate
-        // NOTE remove this when finish adding function
-        this.deviceStock.add(Map.of(Device.builder().name("Hello").build(), 1));
-        this.deviceStock.add(
-                Map.of(
-                        KitchenDevice.builder()
-                                .dimensions(1, 2, 3)
-                                .annualEnergyCost(0)
-                                .name("water dispenser")
-                                .price(100000)
-                                .build(),
-                        2));
-        this.deviceStock.add(Map.of(HomeApplianceDevice.builder().name("Home test").build(), 1));
-        this.deviceStock.add(Map.of(MobileDevice.builder().name("Mobile Test").build(), 1));
-    }
+	// constructor
+	public DeviceManage() {
+		this.deviceStock = new java.util.ArrayList<>(); // initiate
+		// NOTE remove this when finish adding function
+		this.deviceStock.add(Map.of(Device.builder().name("Hello").build(), 1));
+		this.deviceStock.add(
+				Map.of(
+						KitchenDevice.builder()
+								.dimensions(1, 2, 3)
+								.annualEnergyCost(0)
+								.name("water dispenser")
+								.price(100000)
+								.build(),
+						2));
+		this.deviceStock.add(Map.of(HomeApplianceDevice.builder().name("Home test").build(), 1));
+		this.deviceStock.add(Map.of(MobileDevice.builder().name("Mobile Test").build(), 1));
+	}
 
-    // methods
-    public void checkDeviceStock(String nameString) {
-        for (Map<Device, Integer> stock : deviceStock) {
-            for (Map.Entry<Device, Integer> entry : stock.entrySet()) {
-                Device device = entry.getKey();
-                Integer quantity = entry.getValue();
+	// methods
+	public void checkDeviceStock(String nameString) {
+		for (Map<Device, Integer> stock : deviceStock) {
+			for (Map.Entry<Device, Integer> entry : stock.entrySet()) {
+				Device device = entry.getKey();
+				Integer quantity = entry.getValue();
 
-                if (device.nameString.equals(nameString) && quantity == 0) {
-                    stock.put(device, 10);
-                    System.out.println(
-                            "Stock for " + device.nameString + " was empty. Setting stock to 10.");
-                }
-            }
-        }
-    }
+				if (device.nameString.equals(nameString) && quantity == 0) {
+					stock.put(device, 10);
+					System.out.println(
+							"Stock for " + device.nameString + " was empty. Setting stock to 10.");
+				}
+			}
+		}
+	}
 
-    //
-    public void editNumDevice(Client client) {
-        // Iterate through the cart to update stock based on items in the cart
-        for (Map<Device, Integer> cartItem : client.getCart()) {
-            for (Map.Entry<Device, Integer> entry : cartItem.entrySet()) {
-                Device deviceInCart = entry.getKey();
-                int quantityInCart = entry.getValue();
+	//
+	public void editNumDevice(Client client) {
+		// Iterate through the cart to update stock based on items in the cart
+		for (Map<Device, Integer> cartItem : client.getCart()) {
+			for (Map.Entry<Device, Integer> entry : cartItem.entrySet()) {
+				Device deviceInCart = entry.getKey();
+				int quantityInCart = entry.getValue();
 
-                // Iterate through the deviceStock to find and update the stock
-                for (Map<Device, Integer> stock : deviceStock) {
-                    for (Map.Entry<Device, Integer> stockEntry : stock.entrySet()) {
-                        Device deviceInStock = stockEntry.getKey();
-                        int currentStock = stockEntry.getValue();
+				// Iterate through the deviceStock to find and update the stock
+				for (Map<Device, Integer> stock : deviceStock) {
+					for (Map.Entry<Device, Integer> stockEntry : stock.entrySet()) {
+						Device deviceInStock = stockEntry.getKey();
+						int currentStock = stockEntry.getValue();
 
-                        // Check if the device in the cart matches the device in the stock
-                        if (deviceInCart.equals(deviceInStock)) {
-                            if (currentStock >= quantityInCart) {
-                                // Update stock: subtract the quantity added to the cart
-                                stock.put(deviceInStock, currentStock - quantityInCart);
-                                System.out.println(
-                                        quantityInCart
-                                                + " "
-                                                + deviceInCart.nameString
-                                                + "(s) were removed from stock.");
-                            } else {
-                                System.out.println(
-                                        "Not enough stock for "
-                                                + deviceInCart.nameString
-                                                + ". Only "
-                                                + currentStock
-                                                + " left.");
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+						// Check if the device in the cart matches the device in the stock
+						if (deviceInCart.equals(deviceInStock)) {
+							if (currentStock >= quantityInCart) {
+								// Update stock: subtract the quantity added to the cart
+								stock.put(deviceInStock, currentStock - quantityInCart);
+								System.out.println(
+										quantityInCart
+												+ " "
+												+ deviceInCart.nameString
+												+ "(s) were removed from stock.");
+							} else {
+								System.out.println(
+										"Not enough stock for "
+												+ deviceInCart.nameString
+												+ ". Only "
+												+ currentStock
+												+ " left.");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
-    // getter and setter
-    public List<Map<Device, Integer>> getDeviceStock() {
-        return deviceStock;
-    }
+	// getter and setter
+	public List<Map<Device, Integer>> getDeviceStock() {
+		return deviceStock;
+	}
 
-    public void setDeviceStock(List<Map<Device, Integer>> deviceStock) {
-        this.deviceStock = deviceStock;
-    }
+	public void setDeviceStock(List<Map<Device, Integer>> deviceStock) {
+		this.deviceStock = deviceStock;
+	}
 }
